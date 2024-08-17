b7e6ac6a0c01190f795ada1fb82b056a54e42fd8
Viktoriya <viktoriaselkova5@gmail.com>
2024-08-17 16:37:35 +0300

    переменная добавлена

--- aaa.go
+++ aaa.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
+var Myname = "Рики"
+
 func main() {
 	fmt.Println("I love my honey")
 	fmt.Println("I like banana")
 	fmt.Println("1+1")
 	fmt.Println(1 + 1)
+	fmt.Println("Рики")
+
 }
